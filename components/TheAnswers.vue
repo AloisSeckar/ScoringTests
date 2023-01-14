2ec8854bff80ba95be4b0d078d508ef2b82e1784
Alois Sečkár <ellrohir@seznam.cz>
2023-01-14 09:38:59 +0100

    answer checkbox styling

--- components/TheAnswers.vue
+++ components/TheAnswers.vue
@@ -1,13 +1,21 @@
 <template>
     <div class="question-answer">
-        <div v-html="answer1"></div>
-        <input type="checkbox" name="answer" @change="$emit('selectAnswer', 'a', $event.target)" />
-        <div v-html="answer2"></div>
-        <input type="checkbox" name="answer" @change="$emit('selectAnswer', 'b', $event.target)" />
-        <div v-html="answer3"></div>
-        <input type="checkbox" name="answer" @change="$emit('selectAnswer', 'c', $event.target)" />
-        <div v-html="answer4"></div>
-        <input type="checkbox" name="answer" @change="$emit('selectAnswer', 'd', $event.target)" />
+        <div class="question-answer-item">
+            <input id="answer1" type="checkbox" name="answer" @change="$emit('selectAnswer', 'a', $event.target)" />
+            <label for="answer1" v-html="answer1"></label>
+        </div>
+        <div class="question-answer-item">
+            <input id="answer2" type="checkbox" name="answer" @change="$emit('selectAnswer', 'b', $event.target)" />
+            <label for="answer2" v-html="answer2"></label>
+        </div>
+        <div class="question-answer-item">
+            <input id="answer3" type="checkbox" name="answer" @change="$emit('selectAnswer', 'c', $event.target)" />
+            <label for="answer3" v-html="answer3"></label>
+        </div>
+        <div class="question-answer-item">
+            <input id="answer4" type="checkbox" name="answer" @change="$emit('selectAnswer', 'd', $event.target)" />
+            <label for="answer4" v-html="answer4"></label>
+        </div>
     </div>
 </template>
 
