2bbf6ee91c219f37178139529c27102f29716cf3
Alois Sečkár <ellrohir@seznam.cz>
2023-01-24 21:42:25 +0100

    answers cannot be changed when test finished

--- components/TheAnswers.vue
+++ components/TheAnswers.vue
@@ -1,22 +1,22 @@
 <template>
     <div class="question-answer">
         <div class="question-answer-item">
-            <input id="answer1" type="checkbox" name="answer" :checked="answer1checked" 
+            <input id="answer1" type="checkbox" name="answer" :checked="answer1checked" :disabled="inputDisabled"
                 @change="$emit('selectAnswer', 'a', $event.target)" />
             <label for="answer1" v-html="answer1"></label>
         </div>
         <div class="question-answer-item">
-            <input id="answer2" type="checkbox" name="answer" :checked="answer2checked" 
+            <input id="answer2" type="checkbox" name="answer" :checked="answer2checked" :disabled="inputDisabled"
                 @change="$emit('selectAnswer', 'b', $event.target)" />
             <label for="answer2" v-html="answer2"></label>
         </div>
         <div class="question-answer-item">
-            <input id="answer3" type="checkbox" name="answer" :checked="answer3checked" 
+            <input id="answer3" type="checkbox" name="answer" :checked="answer3checked" :disabled="inputDisabled"
                 @change="$emit('selectAnswer', 'c', $event.target)" />
             <label for="answer3" v-html="answer3"></label>
         </div>
         <div class="question-answer-item">
-            <input id="answer4" type="checkbox" name="answer" :checked="answer4checked" 
+            <input id="answer4" type="checkbox" name="answer" :checked="answer4checked" :disabled="inputDisabled"
                 @change="$emit('selectAnswer', 'd', $event.target)" />
             <label for="answer4" v-html="answer4"></label>
         </div>
@@ -37,6 +37,8 @@ const answer1checked = computed(() => currentAnswer.value?.a);
 const answer2checked = computed(() => currentAnswer.value?.b);
 const answer3checked = computed(() => currentAnswer.value?.c);
 const answer4checked = computed(() => currentAnswer.value?.d);
+
+const inputDisabled = computed(() => data.finished)
 
 defineEmits<{
     (e: 'selectAnswer', id: string, element: HTMLInputElement): void
