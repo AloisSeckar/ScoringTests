67ba8610927652d172ef0f81e1ca10c725a00607
Alois Sečkár <ellrohir@seznam.cz>
2023-02-06 20:33:01 +0100

    show top navbox

--- app.vue
+++ app.vue
@@ -3,6 +3,7 @@
     <TheModal v-show="useModalStore().show" />
     <TheHeader />
     <div class="mx-auto p-2 w-full min-[740px]:w-4/5">
+      <TheNav />
       <TheQuestion />
       <TheControls />
       <TheResult v-if="useQuestionStore().finished" />
---
+++ components/TheNav.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="nav-box">
+    Otázka <strong>{{ current }}</strong> z <strong>{{ total }}</strong>
+    /
+    označených: <strong>{{ marked }}</strong>
+  </div>
+</template>
+
+<script setup lang="ts">
+const current = computed(() => useQuestionStore().current + 1)
+const total = computed(() => useQuestionStore().totalQuestions)
+const marked = computed(() => useQuestionStore().answers.filter(a => a.marked).length)
+</script>
